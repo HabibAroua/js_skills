e693dc84b5aa4eb1bc66d64a9ef337e002806cf2
HabibAroua <habib.aroua@sesame.com.tn>
2024-09-09 11:23:02 +0100

    add the object of question and answers

--- quiz_app/script.js
+++ quiz_app/script.js
@@ -1,1 +1,25 @@
-alert("Hello");
+const questions = 
+[
+    {
+        question: "Which is larget animal in the world?",
+        answers:
+        [
+            {
+                text: "Shark",
+                correct: false
+            },
+            {
+                text: "Blue whale",
+                correct: false
+            },
+            {
+                text: "Elephant",
+                correct: false
+            },
+            {
+                text: "Giraffe",
+                correct: false
+            },
+        ]
+    }
+]
