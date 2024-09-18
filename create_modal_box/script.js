96531dabe71711068fe6b53d54d9206261d96485
HabibAroua <habib.aroua@sesame.com.tn>
2024-09-18 23:00:33 +0100

    finish integration

--- create_modal_box/script.js
+++ create_modal_box/script.js
@@ -1,9 +1,11 @@
+let popup = document.getElementById('popup');
+
 function openPopup()
 {
-
+    popup.classList.add('open-popup');
 }
 
 function closePopup()
 {
-    
+    popup.classList.remove('open-popup');
 }
